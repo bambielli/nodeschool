5acff2d89b62b8ede5e1237e9699ec25863f2667
Brian Ambielli <brianambielli2007@u.northwestern.edu>
2013-09-24 23:41:55 -0500

    exercise 4

---
+++ lesson1/async_io.js
@@ -0,0 +1,18 @@
+//my way
+var fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (err, data){
+	if(!err){
+		console.log((data.split('\n').length) -1);
+	}
+});
+
+//the highway
+
+  var fs = require('fs');
+  var file = process.argv[2];
+  
+  fs.readFile(file, function (err, contents) {
+    // fs.readFile(file, 'utf8', callback) can also be used
+    var lines = contents.toString().split('\n').length - 1
+    console.log(lines)
+  });
